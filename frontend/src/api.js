52ada2e5a21daebea79eb10381c5c43a9dfd58a4
DoktorTomato <ivan2006poketrener@gmail.com>
2025-07-08 17:42:54 +0300

    deploy fix

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://educational-analyzer-api-abcde12345-uc.a.run.app/" || "http://localhost:8000",
+  baseURL: "https://educational-analyzer-api-ul26xgluia-ew.a.run.app" || "http://localhost:8000",
   headers: {
     "Content-Type": "application/json"
   }
